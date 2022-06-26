eaca88c11cafaecc09276362a56cc8142903ff62
codebro <64486383+paulsso@users.noreply.github.com>
2022-06-27 01:15:25 +0200

    Create debian-minimal-first.sh

---
+++ debian-minimal-first.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+sudo cp $HOME/git/stackscripts/debian-resource.list /etc/apt/sources.list
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install xorg xserver-xorg nvidia-detect xutils mesa-utils xinit 
+
+sudo apt install nvidia-driver
