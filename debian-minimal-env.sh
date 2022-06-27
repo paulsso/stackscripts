5f45497007766c8a16679092ee9901d9c8979edb
codebro <64486383+paulsso@users.noreply.github.com>
2022-06-27 09:31:14 +0200

    Update debian-minimal-env.sh

--- debian-minimal-env.sh
+++ debian-minimal-env.sh
@@ -1,7 +1,7 @@
-sudo apt install firmware-linux firmware-linux-nonfree
+sudo apt -y install firmware-linux firmware-linux-nonfree
 
-sudo apt install sddm
-sudo apt install make python python3 python3-pip
+sudo apt -y install sddm
+sudo apt -y install make python python3 python3-pip
 
 git clone https://gitlab.com/dwt1/multicolor-sddm-theme.git ../dt-multicolor-sddm-theme
 git clone git clone https://git.suckless.org/dwm ../dwm
@@ -14,4 +14,4 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
     
-sudo apt update && sudo apt install codium
+sudo apt update && sudo apt -y install codium
