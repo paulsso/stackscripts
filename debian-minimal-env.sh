1fd6b38b229294e61c0a05de38d602e50a9a76ba
codebro <64486383+paulsso@users.noreply.github.com>
2022-06-27 10:00:14 +0200

    Update debian-minimal-env.sh

--- debian-minimal-env.sh
+++ debian-minimal-env.sh
@@ -4,10 +4,6 @@ sudo apt -y install sddm
 sudo apt -y install python python3 python3-pip
 sudo apt -y install build-essential libx11-dev libxft-dev libxinerama-dev libfreetype6-dev libfontconfig1-dev
 
-git clone https://gitlab.com/dwt1/multicolor-sddm-theme.git ../dt-multicolor-sddm-theme
-git clone https://git.suckless.org/dwm ../dwm
-git clone https://git.suckless.org/st ../st
-
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
@@ -17,8 +13,23 @@ echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https:/
     
 sudo apt update && sudo apt -y install codium
 
-cd ../dwm
+mkdir $HOME/.suckless
+cd $HOME/.suckless
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/st
+git clone https://git.suckless.org/dmenu
+
+cd $HOME/.suckless/.suckless
+cd dwm
+make
 sudo make clean install
 
-cd ../st
+cd $HOME/.suckless/st
+make
 sudo make clean install
+
+cd $HOME/.suckless/dmenu
+make
+sudo make clean install
+cd 
