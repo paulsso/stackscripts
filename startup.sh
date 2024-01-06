1ea5404db5d64ff27a334db62f72685c57df45e7
codebro <64486383+paulsso@users.noreply.github.com>
2024-01-06 20:42:21 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,11 +4,8 @@ nitrogen --restore &
 exec /usr/local/bin/dwmblocks &
 exec /usr/local/bin/dwm 
 
-<<<<<<< HEAD
-=======
 #Start blocks
 exec /usr/local/bin/dwmblocks &
 
 #Execute DWM
 exec /usr/local/bin/dwm
->>>>>>> 2b5e520b5e276f650549cb2aa0475bd28aaee2d0
